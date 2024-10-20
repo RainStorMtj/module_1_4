cdcc5e208cc840074890b253b7f21eef4b03ced2
RainStorMtj <145570462+RainStorMtj@users.noreply.github.com>
2024-10-20 04:52:40 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input()
+print(len(my_string)) # Вывод количества символов в слове
+print(my_string.upper()) # Вывод в верхнем регистре
+print(my_string.lower()) # Вывод в нижнем регистре
+print(my_string.replace(' ', '')) # Удаление проблеов
+print(my_string[0]) # Вывод первой буквы слова
+print(my_string[-1]) # Вывод последний буквы слова
